e454c9a13fbc7ad7da18dcfad4397673cb3b289d
Tomasz Trebacz <73560937+TomaszTrebacz@users.noreply.github.com>
2020-10-28 20:04:10 +0100

    Add schema validation

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -3,10 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.required(),
+        DB_PORT: Joi.number().default(5432),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.required(),
+        DB_DATABASE: Joi.required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
