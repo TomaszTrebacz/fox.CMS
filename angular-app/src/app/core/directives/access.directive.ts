0eb4cb2cb5382d4777363bd7558cfa83ef705681
Tomasz Trębacz <trebacztomasz@outlook.com>
2021-01-13 23:14:22 +0100

    feat: create custom access directive
    this directive in the same way as auth guard - receives roles and compare with actual user role

---
+++ angular-app/src/app/core/directives/access.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { userRole } from '../enums';
+import { AuthService } from '../services/auth/auth.service';
+
+@Directive({
+  selector: '[access]',
+})
+export class AccessDirective {
+  constructor(
+    private tempRef: TemplateRef<any>,
+    private viewCont: ViewContainerRef,
+    private authService: AuthService
+  ) {}
+
+  @Input('access') set access(roles: userRole[]) {
+    const role = this.authService.userRole;
+
+    if (roles.includes(role)) {
+      this.viewCont.createEmbeddedView(this.tempRef);
+    } else {
+      this.viewCont.clear();
+    }
+  }
+}
