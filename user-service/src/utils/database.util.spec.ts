8e5412acddcdc68258c1ca03cad2ea5cfeecfaf3
Tomasz Trębacz <trebacztomasz@outlook.com>
2021-02-03 16:10:18 +0100

    feat: add unit test for isFound function in database.util #2

--- user-service/src/utils/database.util.spec.ts
+++ user-service/src/utils/database.util.spec.ts
@@ -31,7 +31,7 @@ describe('isExecuted', () => {
   });
 });
 
-describe('isFound', () => {
+describe('isArrayFound', () => {
   describe('if rows exist', () => {
     it('should return noting', () => {
       const res: UserI[] = [
@@ -59,6 +59,37 @@ describe('isFound', () => {
         isArrayFound(res);
       } catch (err) {
         expect(err.message).toEqual('Can not find any data.');
+      }
+    });
+  });
+});
+
+describe('isFound', () => {
+  describe('if row exists', () => {
+    it('should return noting', () => {
+      const res: UserI = {
+        id: '55a1a5be-d03b-48bf-b028-47618cf5b734',
+        email: 'Jan@kowalski.pl',
+        firstName: 'Jam',
+        lastName: 'Kowalski',
+        password:
+          '$argon2i$v=19$m=16,t=2,p=1$c2RhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYQ$Ef+uiuieK/R21dWf9NqPGA',
+        phoneNumber: '+47123456789',
+        created: new Date(),
+        updated: new Date(),
+      };
+
+      expect(isFound(res, 'Can not find user.')).toEqual(undefined);
+    });
+  });
+  describe('otherwise', () => {
+    it('should throw an error', () => {
+      const res = undefined;
+
+      try {
+        expect(isFound(res, 'Can not find user.'));
+      } catch (err) {
+        expect(err.message).toEqual('Can not find user.');
       }
     });
   });
